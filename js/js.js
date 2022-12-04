3caa97d68cbdbd8f3870f035b09cd4a638d29f92
Lucas Wankerl <lucas.wankerl@iu-study.org>
2022-12-04 20:58:00 +0100

    Changes to be committed: 	modified:   index.html 	modified:   js/js.js

--- js/js.js
+++ js/js.js
@@ -34,8 +34,8 @@ function myFunction() {
 }
 
 
-function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
+function sortTable(spalte, button) {
+  var table, rows, switching, i, x, y, shouldSwitch, functionName;
   table = document.getElementById("myTable");
   switching = true;
   /* Make a loop that will continue until
@@ -51,8 +51,8 @@ function sortTable() {
       shouldSwitch = false;
       /* Get the two elements you want to compare,
       one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
+      x = rows[i].getElementsByTagName("TD")[spalte];
+      y = rows[i + 1].getElementsByTagName("TD")[spalte];
       // Check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
         // If so, mark as a switch and break the loop:
@@ -67,4 +67,43 @@ function sortTable() {
       switching = true;
     }
   }
+  functionName="sortTableReverse("+spalte+", '"+button+"')";
+  document.getElementById(button).setAttribute("onclick", functionName);
+}
+
+function sortTableReverse(spalte, button) {
+  var table, rows, switching, i, x, y, shouldSwitch, functionName; 
+  table = document.getElementById("myTable");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[spalte];
+      y = rows[i + 1].getElementsByTagName("TD")[spalte];
+      // Check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        // If so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+  functionName="sortTable("+spalte+", '"+button+"')";
+  document.getElementById(button).setAttribute("onclick", functionName);
 }
